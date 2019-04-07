b49b7e15943558218f4261c266f6be5aaf22bc76
Andrew <andrew-thompson@hotmail.co.uk>
2019-04-07 14:17:57 +0100

    Allow package to be used without express

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-## OSRS json API wrapper
+# OSRS API wrapper
 
+## How to use
+```
+const HiscoresApi = require('osrs-hiscores-api')
+
+HiscoresApi.getStats('name', 'gamemode').then(res => {
+    // getStats returns a promise
+    // so we either need .then
+    // or to use an async function
+})
+```
+
+## Local API:
 This repository uses [node express](https://expressjs.com/) to generate an api to fetch OSRS user stats
 
 Endpoint information once running: [schema.md](schema.md)
 
-To run locally:
+To run the API locally:
 ```
 yarn
 yarn run start
--- index.js
+++ index.js
@@ -1,23 +1,3 @@
 const HiscoresApi = require('./lib/HiscoresApi')
-const express = require('express')
-const app = express()
-const port = 3000
 
-const api = new HiscoresApi()
-
-app.get('/v1/stats/:name', (req, res) => {
-  api
-    .getStats(req.params.name, req.query.mode)
-    .then(data => {
-      res.type('json')
-      res.status(200)
-      res.send(data)
-    })
-    .catch(e => {
-      res.type('json')
-      res.status(e.status)
-      res.send({ message: e.message })
-    })
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = new HiscoresApi()
